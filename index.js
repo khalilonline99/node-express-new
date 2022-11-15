46f0e0adb1cfeac9d3196ca6f5f7b823d34f15ca
khalil <khalilonline99@gmail.com>
2022-11-15 18:34:35 +0600

    office

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Port is working fine')
+})
+
+app.use(cors())
+
+const users = [
+    { id: 1, name: 'Helal', email: 'helal@gmail.com'},
+    { id: 2, name: 'Kamal', email: 'Kamal@gmail.com'},
+    { id: 3, name: 'Belal', email: 'Belal@gmail.com'},
+];
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 
